e53cf7e39aca1bcb4a66e5a612a2dec19763d27d
Nico Burniske <nicoburniske@gmail.com>
2026-09-11 11:03:37 -0400

    delete

--- flake.nix
+++ flake.nix
@@ -21,31 +21,9 @@
         in {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              dbus.dev
-              gst_all_1.gstreamer
-              gst_all_1.gst-plugins-base
-              gst_all_1.gst-plugins-good
-              pipewire
-              pkg-config
+              nushell
               toolchain
             ];
-            GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.makeSearchPath "lib/gstreamer-1.0" (with pkgs; [
-              gst_all_1.gst-plugins-base
-              gst_all_1.gst-plugins-good
-              pipewire
-            ]);
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-              dbus
-              glib
-              gst_all_1.gstreamer
-              gst_all_1.gst-plugins-base
-              libxkbcommon
-              libx11
-              libxcursor
-              libxi
-              libxrandr
-              wayland
-            ]);
           };
         });
     };
